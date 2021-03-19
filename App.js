12298a1d482b2657f9f0fcba4b1ad136de18bb25
Kryspin Turczynski <kjturkman@gmail.com>
2021-03-18 19:42:45 -0700

    private contributions set to show

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>It all starts here!</Text>
+      <Text>Ready to code!</Text>
       <StatusBar style="auto" />
     </View>
   );
